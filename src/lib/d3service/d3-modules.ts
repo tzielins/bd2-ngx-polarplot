b5e41fa76e01c8f780123980f7b396c0cc3fc8c1
T. Zielinski <zajawka@o2.pl>
2018-07-19 18:53:00 +0100

    D3 dependencies update, replacing missing color paletee

--- src/lib/d3service/d3-modules.ts
+++ src/lib/d3service/d3-modules.ts
@@ -1,6 +1,7 @@
 export * from 'd3-array';
 export * from 'd3-axis';
 export * from 'd3-scale';
+export * from 'd3-scale-chromatic';
 export * from 'd3-selection';
 import 'd3-selection-multi';
 export { ValueMap } from 'd3-selection-multi';
--- src/lib/polar-plot/color-palette.ts
+++ src/lib/polar-plot/color-palette.ts
@@ -2,7 +2,28 @@ import {d3} from "../d3service";
 
 export class BD2ColorPalette {
 
-  static extendPalette(palette: string[], size: number): string[] {
+  static schemeCategory20 = ['#1f77b4'
+,'#aec7e8'
+,'#ff7f0e'
+,'#ffbb78'
+,'#2ca02c'
+,'#98df8a'
+,'#d62728'
+,'#ff9896'
+,'#9467bd'
+,'#c5b0d5'
+,'#8c564b'
+,'#c49c94'
+,'#e377c2'
+,'#f7b6d2'
+,'#7f7f7f'
+,'#c7c7c7'
+,'#bcbd22'
+,'#dbdb8d'
+,'#17becf'
+,'#9edae5'];
+
+  static extendPalette(palette: string[] | ReadonlyArray<string>, size: number): string[] {
 
     if (!palette || palette.length === 0) {
       palette = ['black'];
@@ -21,7 +42,7 @@ export class BD2ColorPalette {
     if (size <= d3.schemeCategory10.length) {
       return BD2ColorPalette.extendPalette(d3.schemeCategory10, size);
     } else {
-      return BD2ColorPalette.extendPalette(d3.schemeCategory20, size);
+      return BD2ColorPalette.extendPalette(BD2ColorPalette.schemeCategory20, size);
     }
 
   }
