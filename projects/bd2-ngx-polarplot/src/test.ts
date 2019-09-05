d91599693570159399171feb37e5df57e813dead
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2019-09-05 19:29:48 +0100

    NG update

--- projects/bd2-ngx-polarplot/src/test.ts
+++ projects/bd2-ngx-polarplot/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect';
+// import 'core-js/es7/reflect'; // comment out after upgrade as tests were not starting
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
